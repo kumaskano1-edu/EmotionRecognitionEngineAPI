6262d588e914475db96ef3643d0114652e95842b
Kurmanbek <kumakaraev01@gmail.com>
2021-05-18 19:03:39 -0700

    Ss

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const morgan = require("morgan");
+
+const app = express();  //Create new instance
+
+const PORT = process.env.PORT || 5000; //Declare the port number
+
+app.use(express.json());    //allows us to access request body as req.body
+app.use(morgan("dev"));     //enable incoming request logging in dev mode
+
+app.get("/ping", (req, res) => {   //Define the endpoint
+
+  return res.send({
+    status: "Healthy",
+  });
+
+});
+
+app.listen(PORT, () => {
+  console.log("Server started listening on port : ", PORT);
+});
